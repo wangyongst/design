301092c59598fcca4ceb77d238b8b966e6c2a5db
wangyongst <wangyongst@gmail.com>
2018-06-27 19:14:16 +0800

    home

--- src/main/java/com/myweb/controller/AdminOneController.java
+++ src/main/java/com/myweb/controller/AdminOneController.java
@@ -11,14 +11,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
-@Controller
+@RestController
 @RequestMapping("/admin")
 public class AdminOneController {
 
