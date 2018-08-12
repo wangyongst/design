598e83223a1a4b0cf61cc5e3538cf0819e2aab57
wangyongst <wangyongst@gmail.com>
2018-08-12 19:16:37 +0800

    home

--- src/main/resources/static/js/content-study.js
+++ src/main/resources/static/js/content-study.js
@@ -1,4 +1,7 @@
 $(function () {
+    $('#send').hide();
+    $('#userstable').hide();
+
     $.ajax({
         type: "GET",
         cache: "false",
@@ -34,17 +37,73 @@ $(function () {
             success: function (result) {
                 if (result.status == 1) {
                     $('#lookImg').attr("src", result.data.image);
+
                 }
             }
         });
         $('#myModal').modal('toggle');
     });
 
+    $("#person").click(function () {
+        var selected = select();
+        if (selected == "") {
+            alert("请先选择你要查看的记录");
+            return;
+        }
+        var ids = selected.split(",");
+        if (ids.length > 2) {
+            alert("请选择一条记录");
+            return;
+        }
+        $('#studyhelpid').val(ids[1])
+        $('#helpstable').hide();
+        $('#look').hide();
+        $('#person').hide();
+        $('#send').show();
+        $('#myTable2').bootstrapTable('destroy');
+        $('#myTable2').bootstrapTable({url:"/admin/study/help/list?helpid="+ids[1]});
+        $('#userstable').show();
+    });
+
+    $("#send").click(function () {
+        $('#myModal2').modal('toggle');
+    });
+
     $("#close").click(function () {
         $('#myModal').modal('toggle');
     });
-});
 
+    $("#close2").click(function () {
+        $('#myModal2').modal('toggle');
+    });
+
+    $("#submit2").click(function () {
+        $.ajax({
+            type: "POST",
+            cache: "false",
+            url: "/admin/user/send/message",
+            data: {
+                helpid: $('#studyhelpid').val(),
+                text: $('#content').val()
+            },
+            dataType: "json",
+            success: function (result) {
+                if (result.status == 1) {
+                    alert("发送成功")
+                }
+            }
+        });
+        $('#myModal2').modal('toggle');
+    });
+
+    $("#myModal").on("hidden.bs.modal", function () {
+        clearForm(this);
+    });
+
+    $("#myModal2").on("hidden.bs.modal", function () {
+        clearForm(this);
+    });
+});
 
 function select() {
     var ids = "";
